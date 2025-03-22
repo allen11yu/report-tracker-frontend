d85e121a32640e067ff0f9b05c936be1c6796aad
Allen Yu <yu.junhong02@gmail.com>
2025-03-21 22:25:10 -0700

    Added meta tag to head section

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" data-mantine-color-scheme="light">
+      <head>
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+      </head>
       <body className={inter.className}>
         <MantineProvider>
           <Notifications />
